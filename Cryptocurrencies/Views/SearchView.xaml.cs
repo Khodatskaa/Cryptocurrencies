0346b832ac39a3dca25090b0af767336654e653b
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-09-05 21:09:54 +0300

    Written code for Views and MainWindow

--- Cryptocurrencies/Views/SearchView.xaml.cs
+++ Cryptocurrencies/Views/SearchView.xaml.cs
@@ -1,27 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Cryptocurrencies.Views
 {
-    /// <summary>
-    /// Interaction logic for SearchView.xaml
-    /// </summary>
     public partial class SearchView : Window
     {
         public SearchView()
         {
             InitializeComponent();
+
+            if (string.IsNullOrWhiteSpace(SearchTextBox.Text))
+            {
+                SearchTextBox.Text = "Enter cryptocurrency name";
+                SearchTextBox.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && textBox.Text == "Enter cryptocurrency name")
+            {
+                textBox.Text = string.Empty;
+                textBox.Foreground = Brushes.Black;
+            }
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+            if (textBox != null && string.IsNullOrWhiteSpace(textBox.Text))
+            {
+                textBox.Text = "Enter cryptocurrency name";
+                textBox.Foreground = Brushes.Gray;
+            }
         }
     }
 }
